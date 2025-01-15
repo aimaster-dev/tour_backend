9df94163e08cbf294835afc347b93cae2642a3b4
AmandeepAppdeft <amandeep.singh@appdeft.biz>
2025-01-15 20:32:16 +0530

    Price Serializer changes

--- price/serializers.py
+++ price/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from .models import Price
 
+
 class PriceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Price
-        fields = ["id", "price", "level", "title", "tourplace", "record_time", "record_limit", "snapshot_limit", "created_at", "updated_at"]
+        fields = ["id", "price", "level", "title", "tourplace", "record_time", "record_limit",
+                  "snapshot_limit", "created_at", "updated_at", "features", "product_id"]
 
     def update(self, instance, validated_data):
         for attr, value in validated_data.items():

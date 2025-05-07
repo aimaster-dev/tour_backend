dcd6b053c8db32cd4bdccd93c856706847964a26
AmandeepAppdeft <amandeep.singh@appdeft.biz>
2025-05-07 18:25:58 +0530

    Push notifications testing

--- pushnotification/urls.py
+++ pushnotification/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import PushNotification, NotificationHistory, LoggingTestAPIView
+from .views import PushNotification
 
 urlpatterns = [
     path('send/', PushNotification.as_view(), name='send_notification'),
-    path('history/', NotificationHistory.as_view(), name='notification_history'),
-    path('test-logging/', LoggingTestAPIView.as_view(), name='test_logging'),
+    # path('history/', NotificationHistory.as_view(), name='notification_history'),
+    # path('test-logging/', LoggingTestAPIView.as_view(), name='test_logging'),
 ]

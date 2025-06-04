4b0e7317ccc218efd5110beebdd836db174750e7
unknown <sahilrawat049@gmail.com>
2025-06-04 18:54:10 +0530

    reset limit of video and snapshots whenevets limits reach  0

---
+++ payment/signals.py
@@ -0,0 +1,15 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
+
+from .models import PaymentLogs
+
+@receiver(post_save, sender=PaymentLogs)
+def reset_video_and_snapshot_limit(sender, instance, created, **kwargs):
+    
+    if not created:
+        # Reset the video and snapshot limit for the user
+        if instance.videoremain == 0:
+            instance.videoremain = 20
+        if instance.snapshotremain == 0:
+            instance.snapshotremain = 20
+        instance.save()
